b037337dd0b385265b31d5fa9287b0e8604da439
joaolsalves <joaomail@gmail.com>
2023-05-01 11:34:32 -0300

    Melhorando formatação do Timer

---
+++ AtualizaTudo.bat
@@ -0,0 +1,4 @@
+git add .
+git commit -m "Melhorando formatação do Timer"
+git push
+pause
--- alarmetreino/assets/js/script.js
+++ alarmetreino/assets/js/script.js
@@ -43,6 +43,47 @@ function fnIniciarTimer(){
     ctimer.style.display = "flex";
     ctimer.style.position = "fixed";
         
-    cPrinc.style.display = "none";
+    cPrinc.style.display = "none";   
+}
+
+function fnTimerSair(){
+    var ctimer = document.getElementById("id-ctn-timer");
+    var cPrinc = document.getElementById("id-ctn-principal");
+ 
+     ctimer.style.display = "none";
+              
+     cPrinc.style.display = "flex"; 
+}
+
+function fnExecutarTimer(prep, exer, desc, rep){
+
+
     
+
+    return;
+
+
+    /*
+
+        Calcular o tempo total do exercício
+
+
+        Executar preparação
+
+            Alerta INICIO
+            Executar TimerExercicio
+
+            Validar descanso
+                Se executar descanso = sim
+                    Alerta DESCANSO
+                    Executar TimerDescanso
+                Se não
+                    Alerta FIM DE TREINO
+
+    
+    */
+}
+
+function fnPrep(){
+    return;
 }
