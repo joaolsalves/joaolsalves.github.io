4656d7415beecfce38426b870c986d2958da8df9
joaolsalves <joaomail@gmail.com>
2023-05-11 17:09:07 -0300

    Timer em funcionamento, faltando alguns ajustes funcionais e visuais - Title

--- AtualizaTudo.bat
+++ AtualizaTudo.bat
@@ -1,4 +1,4 @@
 git add .
-git commit -m "Timer em funcionamento, faltando alguns ajustes funcionais e visuais"
+git commit -m "Timer em funcionamento, faltando alguns ajustes funcionais e visuais - Title"
 git push
 pause
